30062155582bbd5d507375b68f73d5186e4374ae
fareed <digital.flowers@hotmail.com>
2024-12-10 21:25:14 +0300

    fix port windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from "react";
+import { VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState<string | null>(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+      <h1>QR Code Scanner</h1>
+      <QrReader
+        onResult={(result, error) => {
+          if (result) {
+            setData(result.getText());
+          }
+
+          if (error) {
+            console.error(error);
+          }
+        }}
+        containerStyle={{ width: "500px", height: "500px" }}
+        constraints={{ facingMode: "environment" }}
+      />
+      <p>Scanned Data: {data}</p>
+    </VStack>
   );
 }
 
